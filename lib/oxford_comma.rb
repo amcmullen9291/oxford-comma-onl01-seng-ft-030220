d8179b10429b676bb9b80e925e02e39f821e0545
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-05 03:53:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
  def oxford_comma(array)
    new_array = []
-     if array.last array.join(" ,and ") 
-     else array.join (", ")
+     if array.last {array.join(" ,and ") 
+     else array.join (", ")}
    end
 end
