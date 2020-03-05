4c689793b21f2b89ea8702d6c3b5943c5e2f2c04
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-05 04:15:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
  def oxford_comma(array)
-     if array.size >1
-        array.join (" and ") 
+   array
+        if array[0]
+          array.join
+         elsif array[-1]
+          array.join (", and ") 
       else
         array.join
      end
