bc97b246c1a6a4dbc2eca09fcd18ca37347d55a5
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-05 04:27:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
- def oxford_comma(array)
-   array.each 
-      if array.lenght= 1
-          array.join
-      elsif array.length= 2 
-          array.join ( "and")
-      elsif array.lenght>=3[]
-          array.join ("") 
-      else
-        array.join
-     end
+def oxford_comma(array)
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    new_last_array_item = "and #{array[-1]}"
+    array.pop
+    array.push(new_last_array_item)
+    return array.join(", ")
   end
+end
