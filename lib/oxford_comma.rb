662ce106a52c247500ba4bf51a070bc088937af7
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-05 04:10:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@
    new_array = []
      if array.size >1
         array.join (" and ") 
-          if array[-1]
+          if array[-1] && array.size>2
             array.join(", and")
           end
       else
