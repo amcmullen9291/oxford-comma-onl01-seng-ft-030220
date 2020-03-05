f2dcf83a098687f5430a72954e64829e06b9a0b5
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-05 04:09:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
  def oxford_comma(array)
    new_array = []
-  array.join (", ")
-     when array.size >1 
-         array.join (" and ") 
-       if array[-1]
-      array.join(", and")
-       else
-     array.join
-        end
+     if array.size >1 
+        array.join (" and ") 
+          if array[-1]
+            array.join(", and")
+          end
+      else
+        array.join
      end
   end
